976e1b58ad64e3751ba5f03e200fe084a108c1df
ArthurBortoleto <arthurmarcelbdossantos@gmail.com>
2024-08-01 11:04:59 -0300

    Criação Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="container">
+      <header>
+        <h1>Profissão Escolhida: Engenheiro Químico</h1>
       </header>
+      <div id="section">
+      <h3>O que é ser um engenheiro químico:</h3>
+        <p>Engenheiros Químicos lidam com amplas transformações, sejam físicas, químicas ou biológicas, em grande escala, envolvendo toneladas ou milhares de litros. Sua principal função é viabilizar a produção industrial de produtos e a aplicação de rotas químicas descobertas por especialistas na área.</p>
+        <img src='https://fiu.edu.br/wp-content/uploads/2022/03/fundo-engenharia-quimica.jpg' alt='#'></img>
+      <div>
+      <h3>Habilidades necessárias para ser um engenheiro químico:</h3>
+        <p>Além da educação formal, habilidades como pensamento analítico, resolução de problemas, habilidades de comunicação, trabalho em equipe e ética profissional são fundamentais para ter sucesso como engenheiro químico.</p>
+      </div>
+      <div>
+      <h3>Oportunidades de carreira:</h3>
+        <ul>
+          <li>Indústria Petroquímica;</li>
+          <li>Setor farmacêutico e biotecnológico;</li>
+          <li>Alimentos e bebidas;</li>
+          <li>Materiais e nanotecnologia;</li>
+          <li>Meio ambiente e energias renováveis;</li>
+          <li>Cosméticos e produtos de higiene pessoal;</li>
+          <li>Indústria de processos químicos;</li>
+          <li>Consultoria e serviços técnicos especializados.</li>
+        </ul>
+      </div>
+      <div>
+      <h3>Ferramentas e tecnologias utilizadas:</h3>
+        <ul>
+          <li>Impressão 3D;</li>
+          <li>Inteligência Artificial;</li>
+          <li>Realidade Aumentada (RA);</li>
+          <li>Realidade Virtual (RV).</li>
+        </ul>
+        </div>
+      </div>
+      <footer>
+      <p>Desenvolvido por Arthur Marcel Bortoleto dos Santos &copy; 2024</p>
+      </footer>
     </div>
   );
 }
